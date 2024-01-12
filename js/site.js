d4d8977b742b9db2d78005e74f9275a0238af32f
Brent Cleary <brent.cleary@gmail.com>
2024-01-11 20:56:40 -0800

    Trees Populating and Numbers Indexing on Card Footer

--- js/site.js
+++ js/site.js
@@ -6,6 +6,13 @@ function GetUserInput()
 
 }
 
+var treeArray = 
+["https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?cs=srgb&dl=pexels-johannes-plenio-1632790.jpg&fm=jpg", 
+"https://media.istockphoto.com/id/1399340544/photo/arboles-y-cielo.jpg?s=612x612&w=0&k=20&c=__yHXv3SRRZXCTz-deFRuV_-QouFfd5gfi6FimqZx5c=", 
+"https://media.istockphoto.com/id/476116580/photo/sycamore-tree-in-summer-field-at-sunset-england-uk.jpg?s=2048x2048&w=is&k=20&c=co7qivS58-PbkvfmwfYS9Zt8f3Qma2LiiNTv292f_KU=",
+"https://media.istockphoto.com/id/1423098235/photo/beautiful-leafy-tree-canopy-on-a-sunny-summers-day.jpg?s=2048x2048&w=is&k=20&c=zufLM2b85vC64WQ1Bh1vuTJJcRC-ai8PYVU9kHvMxec=",
+"https://media.istockphoto.com/id/844226534/photo/leaf-background.jpg?s=2048x2048&w=is&k=20&c=SuFwCpxv6ALde15whmnrtQ42SpuC09aLLwUI2pE5uMM="]
+
 function GenerateCards()
 {
   let tableBody = document.getElementById("tableDisplay");
@@ -23,6 +30,13 @@ function GenerateCards()
     {
       // clone the original div element
       let clonedCard = firstCard.cloneNode(true);
+      
+      // select img src in cloned card
+      let treeImg = clonedCard.querySelector("#plantImg");
+      let treeNumber = clonedCard.querySelector("#plantNumber");
+      // assign src to img in tree array index j
+      treeImg.src = treeArray[j];
+      treeNumber.innerHTML = "Tree # " + (j + 1);
 
       // Insert the cloned div into the table cell
       rowCols[j].appendChild(clonedCard);
